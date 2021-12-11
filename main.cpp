0c70b6be5b317dc31d7c1adae69a7d0bcca41d89
jmisilo <jmisilo@interia.pl>
2021-12-11 22:39:21 +0100

    Implement functions for quicksort

--- main.cpp
+++ main.cpp
@@ -40,6 +40,44 @@ vector<int> toFile(vector<int> vect) {
     out.close();
 }
 
+////////////////////////////////////////////
+// QUICK SORT
+int partition(vector<int> &values, int left, int right) {
+    int pivotIndex = left + (right - left) / 2;
+    int pivotValue = values[pivotIndex];
+
+    int i = left, j = right;
+
+    int temp;
+
+    while(i <= j) {
+        while(values[i] < pivotValue) {
+            i++;
+        }
+        while(values[j] > pivotValue) {
+            j--;
+        }
+        if(i <= j) {
+            temp = values[i];
+            values[i] = values[j];
+            values[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+void quicksort(vector<int> &values, int left, int right) {
+    if(left < right) {
+        int pivotIndex = partition(values, left, right);
+        quicksort(values, left, pivotIndex - 1);
+        quicksort(values, pivotIndex, right);
+    }
+}
+////////////////////////////////////////////
+
 void executionTime(vector<int> vect, bool sortType) {
     // sortType - if True - quicksort, else insertation sort
     string sortName;
